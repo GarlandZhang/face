9ede5ba76aa4b16cffd7b9f10010feee2f74326f
GarlandZhang <garland._zhang@hotmail.com>
2019-03-17 15:09:11 -0400

    update test suite

--- test/helpers/search_filter_test.rb
+++ test/helpers/search_filter_test.rb
@@ -4,13 +4,13 @@ class SearchFilterTest < ActiveSupport::TestCase
     def setup
     end
 
-    test "#search_entities_from_input returns empty array if input is nil" do
-      search_filter = SearchFilter.new(entities: nil, input: nil)
+    test "#search_entities_from_input returns empty array if input is empty" do
+      search_filter = SearchFilter.new(entities: nil, input: "")
       assert_equal [], search_filter.search_entities_from_input
     end
 
-    test "#search_entities_from_input returns empty array if input is empty" do
-      search_filter = SearchFilter.new(entities: nil, input: "")
+    test "#search_entities_from_input returns empty array if entities is nil" do
+      search_filter = SearchFilter.new(entities: nil, input: "foo")
       assert_equal [], search_filter.search_entities_from_input
     end
 end
