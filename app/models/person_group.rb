ac09c83c0cb3cd322d3316d71e2ed0086abc3f64
GarlandZhang <garland._zhang@hotmail.com>
2019-05-06 08:40:19 -0400

    Remove redundant method as it adds indirection

--- app/models/person_group.rb
+++ app/models/person_group.rb
@@ -2,18 +2,6 @@ class PersonGroup < ApplicationRecord
   belongs_to :user
   has_many :people, :dependent => :destroy
 
-  def existing_people(existing_ids)
-    existing_ids.each_with_object([]) do |existing_id, existing_people|
-      candidates = existing_id['candidates']
-      next if candidates.empty?
-      candidate = candidates.first['personId']
-      person = Person.find_by_person_id(candidate)
-      current_face_id = existing_id['faceId']
-      person.last_face_id = current_face_id
-      existing_people << person
-    end
-  end
-
   def add_new_people(new_people)
     people_ids = people.map(&:person_id)
     new_people.each do |new_person|
