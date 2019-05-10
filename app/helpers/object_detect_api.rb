faf30f3c1703ea07c989947af8f5e479d5f140ed
GarlandZhang <garland._zhang@hotmail.com>
2019-05-09 23:06:38 -0400

    Create image_tags endpoint

--- app/helpers/object_detect_api.rb
+++ app/helpers/object_detect_api.rb
@@ -1,3 +1,16 @@
 module ObjectDetectApi
-  
+  URL = 'vision/v2.0/analyze'
+  SUBSCRIPTION_KEY = '0f0c7750313442659facc6bbd506ab08'
+
+  class << self
+    def image_tags(image)
+      ApiSetup.post_call_azure(
+        endpoint_name: URL,
+        request_params: { 'visualFeatures' => 'Description,Tags' },
+        request_body: image,
+        request_type: ApiSetup.REQUEST_TYPE_OS, 
+        subscription_key: SUBSCRIPTION_KEY,
+      )
+    end
+  end
 end
