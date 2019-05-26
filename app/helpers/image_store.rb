52bf384cc7a337e75aeeee837c9cf5709be2f289
GarlandZhang <garland._zhang@hotmail.com>
2019-05-25 23:59:35 -0400

    Complete api call setup for image store api to imgur

--- app/helpers/image_store.rb
+++ app/helpers/image_store.rb
@@ -1,18 +1,28 @@
 module ImageStore
 
-  URL = 'https://api.imgur.com/3'
+  URL = 'https://api.imgur.com/3/'
   client_id = '546c25a59c58ad7'
   client_secret = 'c750216cb24c8f95fbf3a12436f3f6a05d88dadd'
 
 
   self << class
     def upload_image(image)
-      uri = uri_setup(endpoint_name: endpoint_name, request_params: request_params)
-      request = request_setup(request_uri: uri.request_uri, request_type: request_type, request_body: request_body, subscription_key: subscription_key, http_method: http_method)
+      uri = uri_setup(endpoint_name: 'upload', request_params: { 'Authorization' => ('Client-ID ' + client_id) })
+      request = request_setup(request_uri: uri.request_uri, request_type: 'multipart/form-data', request_body: image, http_method: Net::HTTP::Post)
       get_response(uri: uri, request: request)
     end
 
     private
+
+    def get_response(uri:, request:)
+      response_body = Net::HTTP.start(
+        uri.host, 
+        uri.port, 
+        :use_ssl => uri.scheme == 'https',
+      ) { |http| http.request(request) }
+      
+      response_body.blank? ? "" : JSON.parse(response_body)
+    end
 
     def uri_setup(endpoint_name:, request_params:)
       uri = URI(URL + endpoint_name)
@@ -20,6 +30,12 @@ module ImageStore
       uri
     end
 
+    def request_setup(request_uri:, request_type:, request_body:, http_method:)
+      request = http_method.new(request_uri)
+      request['Content-Type'] = "application/#{request_type}"
+      request.body = request_body.to_s
+      request
+    end
     
   end
 end
