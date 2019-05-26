fc2072958829519d1495f71365e032ef03de5ac5
GarlandZhang <garland._zhang@hotmail.com>
2019-05-25 23:41:06 -0400

    Add ImageStore API module

---
+++ app/helpers/image_store.rb
@@ -0,0 +1,12 @@
+module ImageStore
+
+  URL = 'https://api.imgur.com/3'
+  client_id = '546c25a59c58ad7'
+  client_secret = 'c750216cb24c8f95fbf3a12436f3f6a05d88dadd'
+
+
+  self << class
+    
+    
+  end
+end
