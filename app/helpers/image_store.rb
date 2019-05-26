2cbc9b5bd8c68de1aed4a80e1174e1c58ffa0cbf
GarlandZhang <garland._zhang@hotmail.com>
2019-05-25 23:43:13 -0400

    Add template for upload image api call method

--- app/helpers/image_store.rb
+++ app/helpers/image_store.rb
@@ -6,7 +6,20 @@ module ImageStore
 
 
   self << class
-    
+    def upload_image(image)
+      uri = uri_setup(endpoint_name: endpoint_name, request_params: request_params)
+      request = request_setup(request_uri: uri.request_uri, request_type: request_type, request_body: request_body, subscription_key: subscription_key, http_method: http_method)
+      get_response(uri: uri, request: request)
+    end
+
+    private
+
+    def uri_setup(endpoint_name:, request_params:)
+      uri = URI(URL + endpoint_name)
+      uri.query = URI.encode_www_form(request_params)
+      uri
+    end
+
     
   end
 end
